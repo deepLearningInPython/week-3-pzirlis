11839bd455a32df0aa93e809e82937c4d74bcc1a
Paulo Zirlis <paulozrdm@gmail.com>
2025-11-14 16:29:38 +0100

    clearer ex 3

--- tasks.py
+++ tasks.py
@@ -103,10 +103,10 @@ def convolute_2d(input_matrix, kernel_matrix):
 
 # -----------------------------------------------
 # Test case
-input_matrix = np.matrix([[1,2,3,4,5],
+input_matrix = np.array([[1,2,3,4,5],
                           [6,7,8,9,10],
                           [11,12,13,14,15],
                           [16,17,18,19,20],
                           [21,22,23,24,25]])
-kernel_matrix = np.matrix([[-1,0,1],[-1,0,1],[-1,0,1]])
+kernel_matrix = np.array([[-1,0,1],[-1,0,1],[-1,0,1]])
 convolute_2d(input_matrix, kernel_matrix)
